e4cda51703edc401bac3b078f94947bf60139d05
Felipe Manzano <felipemanzanofp@gmail.com>
2022-03-14 18:03:38 -0300

    Criação do End-Point listAllUser

--- src/main/java/com/manzanofp/CrudCompleto/controllers/UserController.java
+++ src/main/java/com/manzanofp/CrudCompleto/controllers/UserController.java
@@ -1,9 +1,14 @@
 package com.manzanofp.CrudCompleto.controllers;
 
+import com.manzanofp.CrudCompleto.model.User;
 import com.manzanofp.CrudCompleto.repositories.UserRepository;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -11,10 +16,17 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping(value = "/listNames/{name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/listName/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public  String listNames (@PathVariable String name){
+    public String listName(@PathVariable String name) {
         return "Deu certo" + name;
     }
+
+    @GetMapping(value = "/listAllUser")
+    public ResponseEntity<List<User>> listAllUser() {
+        List<User> users = userRepository.findAll();
+        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+    }
+
 
 }
