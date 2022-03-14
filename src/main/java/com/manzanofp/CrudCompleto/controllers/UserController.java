92ddc579982ea06a25750f6473dafebc08e89425
Felipe Manzano <felipemanzanofp@gmail.com>
2022-03-14 19:07:32 -0300

    Criação End-Point SaveUser e findById

--- src/main/java/com/manzanofp/CrudCompleto/controllers/UserController.java
+++ src/main/java/com/manzanofp/CrudCompleto/controllers/UserController.java
@@ -2,30 +2,37 @@ package com.manzanofp.CrudCompleto.controllers;
 
 import com.manzanofp.CrudCompleto.model.User;
 import com.manzanofp.CrudCompleto.repositories.UserRepository;
-import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
-
-    @RequestMapping(value = "/listName/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String listName(@PathVariable String name) {
-        return "Deu certo" + name;
-    }
-
+    
     @GetMapping(value = "/listAllUser")
     public ResponseEntity<List<User>> listAllUser() {
         List<User> users = userRepository.findAll();
         return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/listUser/{id}")
+    public ResponseEntity<Optional<User>> findById(@PathVariable Integer id) {
+        Optional<User> obj = userRepository.findById(id);
+        return  ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping(value = "/saveUser")
+    @ResponseBody
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        User obj = userRepository.save(user);
+        return new ResponseEntity<>(obj, HttpStatus.CREATED);
     }
 
 
