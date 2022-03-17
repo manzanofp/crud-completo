ec5b9650da4cbaf6553078906725ee8407ff7979
Felipe Manzano <felipemanzanofp@gmail.com>
2022-03-17 20:11:29 -0300

    Criação do Modal para pesquisar e editar com End-Point

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -7,7 +7,11 @@ var age = $("#age").val();
 $.ajax({
 method: "POST",
 url: "saveUser",
-data: JSON.stringify({id : id, name : name, age : age}),
+data: JSON.stringify({
+id : id,
+name : name,
+ age : age
+ }),
 contentType: "application/json; charset=utf-8",
 
 success: function (response){
@@ -18,6 +22,50 @@ alert("Saved successfully!");
 }).fail(function (xhr, status, errorThrown) {
 alert("error saving: " + xhr.responseText);
 });
+}
 
+function edit(id){
 
+$.ajax({
+method: "GET",
+url: "findUser",
+data: "iduser=" + id,
+
+success: function (response){
+
+$("#id").val(response.id);
+$("#name").val(response.name);
+$("#age").val(response.age);
+
+$('#modal').modal('hide');
+
+}
+}).fail(function (xhr, status, errorThrown) {
+alert("error when searching by id: " + xhr.responseText);
+});
+}
+
+function searchUser(){
+var name = $('#searchUser').val();
+
+if(name != null && name.trim() != ' '){
+
+$.ajax({
+method: "GET",
+url: "findByName",
+data: "name=" + name,
+
+success: function (response){
+$('#tableResult > tbody > tr').remove();
+
+for (var i = 0; i < response.length; i++){
+$('#tableResult > tbody').append('<tr><td>'+response[i].id+'</td><td>'+response[i].name+'</td><td>'+response[i].age+'</td><td><button type="button" class="btn btn-outline-danger" onclick="edit('+response[i].id+')">Edit</button></td></tr>');
+
+}
+
+}
+}).fail(function (xhr, status, errorThrown) {
+alert("error when searching: " + xhr.responseText);
+});
+}
 }
